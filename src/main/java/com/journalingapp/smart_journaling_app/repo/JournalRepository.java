7765d2b6dbf441e4c4fea40e8b648d3c6034ae61
Adi Setiadi Putra <bungady27@gmail.com>
2025-08-31 12:51:29 +0700

    menambahkan method opsional

--- src/main/java/com/journalingapp/smart_journaling_app/repo/JournalRepository.java
+++ src/main/java/com/journalingapp/smart_journaling_app/repo/JournalRepository.java
@@ -1,9 +1,17 @@
 package com.journalingapp.smart_journaling_app.repo;
 
 import com.journalingapp.smart_journaling_app.model.Journal;
+import com.journalingapp.smart_journaling_app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface JournalRepository extends JpaRepository<Journal, UUID> {
+
+    // Cari jurnal berdasarkan User object
+    List<Journal> findByUser(User user);
+
+    // Atau langsung pakai userId
+    List<Journal> findByUserId(UUID userId);
 }
